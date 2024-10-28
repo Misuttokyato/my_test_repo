8d6fae7b6a91a2fc13385a8838ec8438ee461a7b
Максим Сергеев <21m.sergeev@gmail.com>
2024-10-28 23:16:30 +0600

    Update lab_2_task_2.py

--- Лабораторная работа 2/lab_2_task_2.py
+++ Лабораторная работа 2/lab_2_task_2.py
@@ -1,16 +1,13 @@
-money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
 
-months = 0
+money_capital = 0
 
-
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-while money_capital + salary > spend:
-    months += 1
-    money_capital += salary - spend
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+for _ in range(months):
+    money_capital += spend - salary
     spend *= 1 + increase
 
-
-print("Количество месяцев, которое можно протянуть без долгов:", months)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
